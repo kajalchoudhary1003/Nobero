dc2a7306137d6606369aec43ac94baa81be0e731
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2024-08-22 21:58:37 +0530

    svg webpack and marquee updates

---
+++ frontend/components/Navbar.js
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+
+function Navbar() {
+  return (
+    <div>
+        <div className="logo">
+<Image src="/Nobero_logo_1_2.svg" alt="logo" width={100} height={100} />
+        </div>
+        <div className="nav-items">
+
+        </div>
+        <div className="extra">
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
